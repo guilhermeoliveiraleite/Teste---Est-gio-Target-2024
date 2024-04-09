f2d2c1978394839df6e593654c4dec2901fbaf60
Guilherme Leite <156142511+guilhermeoliveiraleite@users.noreply.github.com>
2024-04-09 19:15:34 -0300

    Add files via upload

---
+++ estagio-ribeirao-preto-target-2024/main.js
@@ -0,0 +1,7 @@
+import sumResult from './scripts/sumResult.js';
+import isFibonacci from './scripts/isFibonacci.js';
+import reverseWord from './scripts/reverseWord.js';
+
+sumResult();
+isFibonacci();
+reverseWord();
